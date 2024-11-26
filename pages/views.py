cfe4032b75894559ef622160e287eb7727216e3f
lesquel <holahi23holahi23@gmail.com>
2024-11-26 18:20:45 -0500

    create header

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,5 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def home_view(request):
-    return render(request, 'pages/pages/home.html')
+    is_cliente = request.user.groups.filter(name='Cliente').exists()
+    return render(request, 'pages/pages/home.html', {'is_cliente': is_cliente})
