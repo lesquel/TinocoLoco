0148a9c1e490a93c9e2f26b0665ba3ea4e198228
lesquel <holahi23holahi23@gmail.com>
2024-11-26 19:00:12 -0500

    footer

--- TinocoLoco/settings.py
+++ TinocoLoco/settings.py
@@ -141,6 +141,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Si tienes una carpeta 'static' a nivel de proyecto
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
