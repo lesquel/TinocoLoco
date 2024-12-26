998bc0024fc344e9533c309fb844c58894cb7844
SteveSant26 <Bryanmenoscal2005@gmail.com>
2024-12-25 22:40:00 -0500

    .

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import environ
 
-
+# Importar configuraciones de varios módulos
 from .jazzmin_settings import *
 from .apps_settings import *
 from .middleware_settings import *
@@ -12,43 +12,43 @@ from .auth_settings import *
 from .templates_settings import *
 from .rest_framework_settings import *
 
+# Inicializar variables de entorno
 env = environ.Env(DEBUG=(bool, False))
 
+# Leer variables de entorno desde un archivo .env
 environ.Env.read_env()
 
-
+# Definir el directorio base del proyecto
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# SECURITY WARNING: keep the secret key used in production secret!
+# ADVERTENCIA DE SEGURIDAD: mantener la clave secreta utilizada en producción en secreto
 SECRET_KEY = env("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# ADVERTENCIA DE SEGURIDAD: no ejecutar con debug activado en producción
 DEBUG = env("DEBUG", default=False)
 
+# Definir los hosts permitidos para la aplicación
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 
-
+# Definir los orígenes permitidos para CORS
 CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS")
 
-
+# Definir la configuración de URL raíz
 ROOT_URLCONF = "core.urls"
 
-
+# Definir la aplicación WSGI
 WSGI_APPLICATION = "core.wsgi.application"
 
-
-
-
-
-
-
+# Definir la URL para servir archivos estáticos
 STATIC_URL = '/static/'
+
+# Definir los directorios donde se encuentran los archivos estáticos
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  
 ]
+
+# Definir el directorio donde se recopilarán los archivos estáticos
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-
+# Definir el campo automático predeterminado para los modelos
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
