d5e17f64d9f3975fc1a039db37c419557940bcf8
SteveSant26 <Bryanmenoscal2005@gmail.com>
2024-12-27 16:44:04 -0500

    .

--- backend/base/utils/errors/event_rentals_retaled.py
+++ backend/base/utils/errors/event_rentals_retaled.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 ALREADY_CONFIRMED = _("Este alquiler ya ha sido confirmado")
-MIN_ATTENDEES_CANNOT_BE_GREATER_THAN_MAX_ATTENDEES = _("El numero de asistentes minimo no puede ser mayor al numero de asistent")
+MIN_ATTENDEES_CANNOT_BE_GREATER_THAN_MAX_ATTENDEES = _("El numero de asistentes minimo no puede ser mayor al numero de asistentes maximo")
 
 class InvalidMinAttendeesError(BaseError):
     def __init__(self, message=MIN_ATTENDEES_CANNOT_BE_GREATER_THAN_MAX_ATTENDEES, identifier="event_rental_min_attendees"):
