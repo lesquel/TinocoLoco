5702dbc8d14667b41af78d07c3bbd68df88aa0f5
SteveSant26 <Bryanmenoscal2005@gmail.com>
2024-12-26 21:08:02 -0500

    .

--- backend/apps/business_configuration/context_processors.py
+++
@@ -1,9 +0,0 @@
-from .models import BusinessConfiguration
-
-
-def business_configuration(request):
-    configuration, _ = BusinessConfiguration.objects.get_or_create()
-    print(configuration)
-    return {
-        "business_configuration":configuration,
-    }
