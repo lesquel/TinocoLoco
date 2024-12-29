87660fbbe9fe1e8011103f884a46b4c3bb0598e2
SteveSant26 <Bryanmenoscal2005@gmail.com>
2024-12-28 22:22:43 -0500

    .

--- backend/base/system_services/service_service.py
+++ backend/base/system_services/service_service.py
@@ -7,6 +7,3 @@ from base.abstracts import AAnaliticService
 class ServiceService(AAnaliticService):
     model = Service
     
-    @classmethod
-    def get_most_populars(self, params="event_rental_services"):
-        return super().get_most_populars(params)
--- front/components/utils/reviews/ReviewList.tsx
+++ front/components/utils/reviews/ReviewList.tsx
@@ -26,7 +26,7 @@ const UserComponent = ({ idUser }: { idUser: number }) => {
   }
   return (
     <h3 className="text-lg font-semibold">
-      Usuario {userData?.username || userData?.first_name}
+      {userData?.username || userData?.first_name}
     </h3>
   );
 };
