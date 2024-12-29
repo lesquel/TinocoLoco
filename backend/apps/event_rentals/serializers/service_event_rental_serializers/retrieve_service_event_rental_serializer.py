4db0ff327e1638280d4de5a7d60cf77fcbcd826a
SteveSant26 <Bryanmenoscal2005@gmail.com>
2024-12-29 01:16:57 -0500

    .

--- backend/apps/event_rentals/serializers/service_event_rental_serializers/retrieve_service_event_rental_serializer.py
+++ backend/apps/event_rentals/serializers/service_event_rental_serializers/retrieve_service_event_rental_serializer.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
+from apps.services.serializers import ServiceSerializer
 from ...models import ServicesEventRental
 
 
 class RetrieveServiceEventRentalSerializer(serializers.ModelSerializer):
     price = serializers.DecimalField(max_digits=10, decimal_places=2)
+    service = ServiceSerializer()
     class Meta:
         model = ServicesEventRental
         fields = "__all__"
