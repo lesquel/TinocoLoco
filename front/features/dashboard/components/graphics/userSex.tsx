2fe58b676319ee5919f5829db55b0f1d50b30b38
lesquel <holahi23holahi23@gmail.com>
2024-12-29 18:40:26 -0500

    csdwcde

--- front/features/dashboard/components/graphics/userSex.tsx
+++ front/features/dashboard/components/graphics/userSex.tsx
@@ -3,46 +3,40 @@ import { TitleSection } from "@/components/utils/titleSection";
 import ReactECharts from "echarts-for-react";
 export function UserSexGraphic() {
   const option = {
-    tooltip: {
-      trigger: "item",
+    xAxis: {
+      data: ["M", "F"]
     },
-    legend: {
-      orient: "vertical",
-      left: "left",
-      data: ["Usuario verificado", "Usuario no verificado"],
-      textStyle: {
-        color: "#fff",
-      },
-    },
+    yAxis: {},
     series: [
       {
-        name: "Usuarios",
-        type: "pie",
-        radius: ["40%", "70%"],
-        avoidLabelOverlap: false,
-        label: {
-          show: false,
-          position: "center",
-        },
-        emphasis: {
-          label: {
-            show: true,
-            fontWeight: "bold",
-          },
-        },
-        labelLine: {
-          show: false,
-        },
+        type: 'bar',
         data: [
-          { value: 335, name: "Usuario verificado" },
-          { value: 310, name: "Usuario no verificado" },
+          10,
+          {
+            value: 43,
+            // Specify the style for single bar
+            itemStyle: {
+              color: '#91cc75',
+              shadowColor: '#91cc75',
+              borderType: 'dashed',
+              opacity: 0.5
+            }
+          },
         ],
-      },
-    ],
+        itemStyle: {
+          barBorderRadius: 5,
+          borderWidth: 1,
+          borderType: 'solid',
+          borderColor: '#73c0de',
+          shadowColor: '#5470c6',
+          shadowBlur: 3
+        }
+      }
+    ]
   };
   return (
     <div className="flex flex-col items-center justify-center max-w-[400px] mx-auto">
-      <TitleSection title="Usuarios " description="verificados"  />
+      <TitleSection title="Sexo" description=" de los usuarios"  />
       <ReactECharts option={option} className="w-full h-full" />
     </div>
   );
--- front/features/dashboard/section/home.tsx
+++ front/features/dashboard/section/home.tsx
@@ -1,14 +1,43 @@
-import {UserActiveGraphic} from "../components/graphics/userActive";
+"use client";
+import { TitleSection } from "@/components/utils/titleSection";
+import { UserActiveGraphic } from "../components/graphics/userActive";
 import { UserEmailVerificate } from "../components/graphics/userEmailVerificate";
+import { UserSexGraphic } from "../components/graphics/userSex";
+import { Card, CardBody, Tab, Tabs } from "@nextui-org/react";
 
 export default function HomeDashboard() {
   return (
-    <div className="flex flex-col items-center justify-center w-full max-w-6xl mx-auto">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
-        <UserActiveGraphic />
-        <UserEmailVerificate />
-        <UserEmailVerificate />
-      </div>
+    <div className="flex w-full flex-col m-9">
+      <Tabs aria-label="Options">
+        <Tab key="users" title="Users">
+          <Card>
+            <CardBody>
+              <TitleSection title="Graficos" description="Usuarios" />
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
+                <UserActiveGraphic />
+                <UserEmailVerificate />
+                <UserSexGraphic />
+              </div>
+            </CardBody>
+          </Card>
+        </Tab>
+        <Tab key="Services" title="Services">
+          <Card>
+            <CardBody>
+            <TitleSection title="Graficos" description="Usuarios" />
+
+            </CardBody>
+          </Card>
+        </Tab>
+        <Tab key="Events" title="Events">
+          <Card>
+            <CardBody>
+              
+              
+            </CardBody>
+          </Card>
+        </Tab>
+      </Tabs>
     </div>
   );
 }
