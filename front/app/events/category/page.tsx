3c5cd0a5a28efca0bad935096d175d3e20ee3e9a
lesquel <holahi23holahi23@gmail.com>
2024-12-29 21:16:57 -0500

    aaaa

--- front/app/events/category/page.tsx
+++ front/app/events/category/page.tsx
@@ -1,9 +1,16 @@
 import { Container } from "@/components/sections/layout/container";
+import { Section } from "@/components/sections/layout/section";
+import { AllCategorysEvent } from "@/features/events/section/events/AllCategorysEvent";
 
 export default async function CategotyEvent() {
   return (
     <Container>
-      <h1>Categoría: </h1>
+      <Section>
+        <AllCategorysEvent
+          size={10}
+          infoComponent={{ title: "Todos", description: "Categorias" }}
+        />
+      </Section>
     </Container>
   );
 }
