76fb45a79d35141891e7d99affe9680e44ba728a
SteveSant26 <Bryanmenoscal2005@gmail.com>
2024-12-29 21:18:45 -0500

    .

--- front/app/events/category/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "@/components/sections/layout/container";
-
-export default async function CategotyEvent() {
-  return (
-    <Container>
-      <h1>Categoría: </h1>
-    </Container>
-  );
-}
